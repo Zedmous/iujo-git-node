7e02b08e2480dac7926862f8f8830c2ef9de425d
zedmous <ejnsilva29@gmail.com>
2022-04-04 20:33:39 -0400

    finalizando 101. Express basado en clases

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
-
 require('dotenv').config();
-const express = require('express')
-const app = express()
-app.get('/',  (req, res)=> {
-  res.send('Hello World')
-})
+const Server = require('./models/server');
 
-app.listen(process.env.PORT,()=>{
-    console.log("Servidor corriendo en puerto",process.env.PORT)
-})
+
+const serve= new Server();
+serve.listen();
